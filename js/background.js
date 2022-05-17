51b1451fe7a57b12af252604c80ba59dbc85aefe
NiJun <87179471@qq.com>
2022-05-17 18:47:43 +0800

    url 清理设置

--- js/background.js
+++ js/background.js
@@ -1,39 +1,34 @@
-console.log('//')
+// chrome.tabs.onRemoved.addListener((tabId, b) => {
+//   console.log(tabId)
+//   // chrome.tabs.get(tabId, tab => {
+//   //   console.log(tab)
+//   // })
+// })
 
-var urlOptions = [
-  { url: 'https://javlibrary.com', clear: ['history', 'local', 'cookies'] }
-]
-
-chrome.storage.local.get(['urlOptions'], function (result) {
-  if (!result.urlOptions) {
-    chrome.storage.local.set({'urlOptions': urlOptions}, function() {
-      console.log('Value is set');
-    })
-  } else {
-    console.log(result.urlOptions)
-  }
+chrome.windows.onCreated.addListener(() => {
+  clearHistory()
 })
 
-// chrome.history.deleteUrl({ url: 'https://www.javlibrary.com/' }, function(result) {
-//   console.log(result)
-// })
+function clearHistory (url = '') {
+  chrome.storage.local.get(['urlOptions'], function (result) {
+    let urlOptions = result.urlOptions || []
 
-// chrome.history.getVisits({ url: 'https://www.javlibrary.com/cn/vl_update.php' }, res => {
-//   console.log(res)
-// })
+    if (url && urlOptions.find(urlOption => urlOption.url === url)) {
+      urlOptions = [urlOptions.find(urlOption => urlOption.url === url)]
+    }
 
-chrome.history.search({
-  text: 'javlibrary.com',
-  startTime: Date.now() - (1000 * 60 * 60 * 24 * 30 * 12 * 3),
-  endTime: Date.now()
-}, res => {
-  console.log(res)
-  if (res && res.length) {
-    res.forEach(item => {
-      console.log(item)
-      chrome.history.deleteUrl({ url: item.url }, () => {
-        console.log('??')
+    urlOptions.forEach(urlOption => {
+      chrome.history.search({
+        text: urlOption.url,
+        startTime: Date.now() - (1000 * 60 * 60 * 24 * 30 * 12 * 3),
+        endTime: Date.now()
+      }, res => {
+        if (res && res.length) {
+          res.forEach(item => {
+            chrome.history.deleteUrl({ url: item.url })
+          })
+        }
       })
     })
-  }
-})
+  })
+}
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,70 @@
-console.log('??')
+let urlOptions = [] // 清理配置项
+let currentUrl = '' // 当前url host
 
+chrome.storage.local.get(['urlOptions'], function (result) {
+  urlOptions = result.urlOptions
+  showCurrentOptions()
+})
 
-chrome.tabs.query({ active: true, currentWindow: true }, (res) => {
-  if (res[0] && res[0].url) {
-    let url = new URL(res[0].url)
-    if (url.host) {
-      $('#cruuent-url').html(url.host)
+// 回显配置
+function showCurrentOptions() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (res) => {
+    if (res[0] && res[0].url) {
+      let urlObj = new URL(res[0].url)
+      if (urlObj.host) {
+        currentUrl = urlObj.host
+        $('#cruuent-url').html(currentUrl)
+        let currentOptions = urlOptions.find(item => item.url === currentUrl)
+        if (currentOptions) {
+          currentOptions.clear.forEach(item => {
+            $(`input[name='${item}']`).attr("checked", true)
+          })
+        }
+      }
     }
+  })
+}
+
+// 更新配置
+$('#submit-btn').click(() => {
+  let findIndex = urlOptions.findIndex(item => item.url === currentUrl)
+
+  let clear = []
+  $("input[type='checkbox']:checked").each((index, item) => {
+    clear.push($(item).attr('name'))
+  })
+
+  if (findIndex === -1) {
+    if (clear.length) {
+      urlOptions.push({clear: [...clear], url: currentUrl })
+      chrome.storage.local.set({'urlOptions': urlOptions})
+      showMessage('success', '已添加到清理名单')
+    } else {
+      showMessage('warning', '请选择清理项')
+    }
+  } else {
+    if (clear.length) {
+      urlOptions.splice(findIndex, 1, { clear: [...clear], url: currentUrl })
+      showMessage('success', '已修改清理配置')
+    } else {
+      urlOptions.splice(findIndex, 1)
+      showMessage('success', '已移除清理名单')
+    }
+    chrome.storage.local.set({'urlOptions': urlOptions})
   }
 })
+
+let showMessageTimer = null
+function showMessage (type = 'info', content = '提示', duration = 3000) {
+  $('#message').removeClass('info success warning error').addClass(type).html(content).show()
+
+  if (showMessageTimer) {
+    clearTimeout(showMessageTimer)
+    showMessageTimer = null
+  }
+
+  showMessageTimer = setTimeout(() => {
+    showMessageTimer = null
+    $('#message').hide()
+  }, 3000)
+}
